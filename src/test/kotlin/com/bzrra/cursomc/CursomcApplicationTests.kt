175253a942d3d1368fb544ad7c2649c4431b2809
Lucas Bezerra da Silva <lucasbezerra06@gmail.com>
2020-07-05 15:47:32 -0300

    Projeto criado

---
+++ src/test/kotlin/com/bzrra/cursomc/CursomcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bzrra.cursomc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
