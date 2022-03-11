9b174005f621d31c484b4dbfce9f65e037c1d7c2
Lucas Bezerra da Silva <lucasbezerra06@gmail.com>
2022-03-11 16:54:43 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/lucasbezerra/cursomc/damain/Categoria.java
@@ -0,0 +1,44 @@
+package com.lucasbezerra.cursomc.damain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private Integer id;
+    private String nome;
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
