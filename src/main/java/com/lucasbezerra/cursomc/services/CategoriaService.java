99f2bdd58b5fd771d0a2e47a361d291771905485
Lucas Bezerra da Silva <lucasbezerra06@gmail.com>
2022-03-18 11:54:17 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/lucasbezerra/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucasbezerra/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.lucasbezerra.cursomc.services;
 
 import com.lucasbezerra.cursomc.damain.Categoria;
 import com.lucasbezerra.cursomc.repositories.CategoriaRepository;
+import com.lucasbezerra.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+ id + ", Tipo: " + Categoria.class.getName()));
     }
 }
