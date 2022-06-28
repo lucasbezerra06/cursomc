8cceba8c5fd68201abd77a4c861ac846268078f8
Lucas Bezerra da Silva <lucasbezerra06@gmail.com>
2022-06-28 11:56:54 -0300

    Email HTML

--- src/main/java/com/lucasbezerra/cursomc/services/EmailService.java
+++ src/main/java/com/lucasbezerra/cursomc/services/EmailService.java
@@ -3,7 +3,11 @@ package com.lucasbezerra.cursomc.services;
 import com.lucasbezerra.cursomc.damain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
