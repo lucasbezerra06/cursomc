b3bc18594d6461730d52263b2acb511aa2e2bfad
Lucas Bezerra da Silva <lucasbezerra06@gmail.com>
2022-06-29 10:26:23 -0300

    Adicionando senha a Cliente

--- src/main/java/com/lucasbezerra/cursomc/domain/Cliente.java
+++ src/main/java/com/lucasbezerra/cursomc/domain/Cliente.java
@@ -21,6 +21,9 @@ public class Cliente implements Serializable {
     private String cpfOuCnpj;
     private Integer tipo;
 
+    @JsonIgnore
+    private String senha;
+
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
@@ -34,12 +37,13 @@ public class Cliente implements Serializable {
 
     public Cliente(){}
 
-    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.cpfOuCnpj = cpfOuCnpj;
         this.tipo = (tipo == null) ? null : tipo.getCod();
+        this.senha = senha;
     }
 
     public Integer getId() {
@@ -56,6 +60,14 @@ public class Cliente implements Serializable {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
     public String getEmail() {
--- src/main/java/com/lucasbezerra/cursomc/services/DBService.java
+++ src/main/java/com/lucasbezerra/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import com.lucasbezerra.cursomc.domain.enums.EstadoPagamento;
 import com.lucasbezerra.cursomc.domain.enums.TipoCliente;
 import com.lucasbezerra.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
@@ -13,6 +14,9 @@ import java.util.Arrays;
 
 @Service
 public class DBService {
+
+    @Autowired
+    BCryptPasswordEncoder pe;
 
     @Autowired
     private CategoriaRepository categoriaRepository;
@@ -93,7 +97,7 @@ public class DBService {
         estadoRepository.saveAll(Arrays.asList(est1, est2));
         cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-        Cliente cli1 = new Cliente(null, "Maria Silva", "lucasbezerra.silva06@outlook.com", "36378912377", TipoCliente.PESSOAFISICA);
+        Cliente cli1 = new Cliente(null, "Maria Silva", "lucasbezerra.silva06@outlook.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123456"));
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
         Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
