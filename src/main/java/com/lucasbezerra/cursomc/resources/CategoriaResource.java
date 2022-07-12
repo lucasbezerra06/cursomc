ba11f8f53fcb0b0174e6bb4dd4ea2f763bf878e1
Lucas Bezerra da Silva <lucasbezerra06@gmail.com>
2022-07-12 08:45:24 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/lucasbezerra/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucasbezerra/cursomc/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import com.lucasbezerra.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -45,6 +46,7 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(listPageDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){
         Categoria obj = service.fromDTO(objDto);
@@ -53,6 +55,7 @@ public class CategoriaResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody CategoriaDTO objDto){
         Categoria obj = service.fromDTO(objDto);
@@ -61,6 +64,7 @@ public class CategoriaResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id){
         service.delete(id);
--- src/main/java/com/lucasbezerra/cursomc/resources/ClienteResource.java
+++ src/main/java/com/lucasbezerra/cursomc/resources/ClienteResource.java
@@ -7,6 +7,7 @@ import com.lucasbezerra.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,6 +29,7 @@ public class ClienteResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<ClienteDTO>> findAll(){
         List<Cliente> list = service.findAll();
@@ -35,6 +37,7 @@ public class ClienteResource {
         return ResponseEntity.ok().body(listDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/page", method = RequestMethod.GET)
     public ResponseEntity<Page<ClienteDTO>> findPage(
             @RequestParam(name = "page", defaultValue = "0") Integer page,
@@ -64,6 +67,7 @@ public class ClienteResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete (@PathVariable Integer id){
         service.delete(id);
