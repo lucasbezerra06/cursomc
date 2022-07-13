4d8190e6a338b222d9d4dc82e0267502060dd63a
Lucas Bezerra da Silva <lucasbezerra06@gmail.com>
2022-07-13 17:15:26 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/lucasbezerra/cursomc/CursomcApplication.java
+++ src/main/java/com/lucasbezerra/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.lucasbezerra.cursomc;
 
+import com.lucasbezerra.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {}
+    public void run(String... args) throws Exception {
+        s3Service.uploadFile("/home/bezerra/Pictures/Wallpapers/Anime/8f6698c006efcf0230f56770be61923a.jpg");
+    }
 }
